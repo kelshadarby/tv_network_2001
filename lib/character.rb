236adf14f4f896393298df801728a7c1924e1f27
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-11 10:22:44 -0700

    Add character class

--- lib/character.rb
+++ lib/character.rb
@@ -1,1 +1,11 @@
 
+class Character
+  attr_reader :info, :name, :actor, :salary
+
+  def initialize(info)
+    @info = info
+    @name = info[:name]
+    @actor = info[:actor]
+    @salary = info[:salary]
+  end
+end
