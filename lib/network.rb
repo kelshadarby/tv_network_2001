8347abb54274420adf7f9e3d810fd97d02fb31ae
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-11 10:22:59 -0700

    Add network class

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,29 @@
 
+class Network
+
+  attr_reader :name, :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    characters = @shows.map { |show| show.characters }.flatten!
+  end
+
+  def actors_by_show
+    @shows.reduce({}) do |actors_hash, show|
+      if actors_hash.has_key?(show)
+        actors_hash[show] << show.actors
+      else
+        actors_hash[show] = show.actors
+      end
+      actors_hash
+    end
+  end
+end
